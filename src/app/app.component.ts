e536368157028bda77b0778defb333eea1140705
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-04-18 19:06:08 -0500

    First stable version XD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { products } from './products';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+
+  items = ['nicolas', 'julian', 'perez'];
+
+  products = products;
+
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
+
+  clickProduct(productId: number) {
+    console.log('Product ID ', productId);
+  }
 }
